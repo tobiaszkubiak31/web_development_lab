047cba75cbb87c6593a93c3f00009296165a83ac
Patryk.Stepien <patryk.stepien@automotivedev.pl>
2020-11-01 17:28:54 +0100

    Added validateUser() member function of AuthService which checks if user with username and password exists

--- backend/src/auth/auth.service.ts
+++ backend/src/auth/auth.service.ts
@@ -1,4 +1,18 @@
 import { Injectable } from '@nestjs/common';
+import { UsersService } from 'src/users/users.service';
+import * as bcrypt from 'bcrypt';
 
 @Injectable()
-export class AuthService {}
+export class AuthService {
+    constructor(private usersService: UsersService) {}
+
+    async validateUser(username: string, password: string): Promise<any> {
+        const foundUser = await this.usersService.findOne(username); // Check if user exists
+        if (foundUser && bcrypt.compare(password, foundUser.password)) { // comparing password with encrypted password
+            const { password, ...result } = foundUser; // this extracted result type which is User type except password
+            return result; // {userId, username};
+        }
+
+        return null;
+    }
+}

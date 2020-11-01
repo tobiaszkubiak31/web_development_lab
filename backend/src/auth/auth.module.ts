8462625f4688c4abc0fa45c31d117e68e7982d56
Patryk.Stepien <patryk.stepien@automotivedev.pl>
2020-11-01 18:24:50 +0100

    Implemented login() member function of UsersService, added JwtStrategy and dotenv dependency

--- backend/src/auth/auth.module.ts
+++ backend/src/auth/auth.module.ts
@@ -4,10 +4,15 @@ import { AuthService } from './auth.service';
 import { PassportModule } from '@nestjs/passport';
 import { LocalStrategy } from './local.strategy';
 import { AuthController } from './auth.controller';
+import { JwtModule } from '@nestjs/jwt';
+import { JwtStrategy } from './jwt.strategy';
 
 @Module({
-  imports: [UsersModule, PassportModule],
-  providers: [AuthService, LocalStrategy],
+  imports: [UsersModule, PassportModule, JwtModule.register({
+    secret: process.env.JWT_SECRET,
+    signOptions: { expiresIn: '60s' }
+  })],
+  providers: [AuthService, LocalStrategy, JwtStrategy],
   controllers: [AuthController]
 })
 export class AuthModule {}
--- backend/src/auth/auth.service.ts
+++ backend/src/auth/auth.service.ts
@@ -1,10 +1,14 @@
 import { Injectable } from '@nestjs/common';
 import { UsersService } from 'src/users/users.service';
 import * as bcrypt from 'bcrypt';
+import { JwtService } from '@nestjs/jwt';
 
 @Injectable()
 export class AuthService {
-    constructor(private usersService: UsersService) {}
+    constructor(
+        private usersService: UsersService,
+        private jwtService: JwtService
+    ) {}
 
     async validateUser(username: string, password: string): Promise<any> {
         const foundUser = await this.usersService.findOne(username); // Check if user exists
@@ -14,5 +18,13 @@ export class AuthService {
         }
 
         return null;
+    }
+
+    async login(user: any): Promise<any> {
+        const payload = { username: user.username, sub: user.userId };
+
+        return {
+            access_token: this.jwtService.sign(payload);
+        };
     }
 }

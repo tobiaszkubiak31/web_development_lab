773b0263795b1d7b92ae86072183113b7af39faf
Patryk.Stepien <patryk.stepien@automotivedev.pl>
2020-11-01 17:08:00 +0100

    Added temporary User type (in future will be User entity instead of this)

--- backend/src/users/users.service.ts
+++ backend/src/users/users.service.ts
@@ -1,4 +1,11 @@
 import { Injectable } from '@nestjs/common';
 
+// temporary User type (in future will be User entity instead of this)
+export type User = {
+    userId: number,
+    username: string,
+    password: string
+};
+
 @Injectable()
 export class UsersService {}

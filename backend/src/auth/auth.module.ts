af9b37ca32f0c7794276ee370cb88712bc54a3cd
Patryk.Stepien <patryk.stepien@automotivedev.pl>
2020-11-01 17:44:05 +0100

    Added LocalStrategy class to connect nestjs with passport-local strategy

--- backend/src/auth/auth.module.ts
+++ backend/src/auth/auth.module.ts
@@ -1,9 +1,11 @@
 import { Module } from '@nestjs/common';
 import { UsersService } from 'src/users/users.service';
 import { AuthService } from './auth.service';
+import { PassportModule } from '@nestjs/passport';
+import { LocalStrategy } from './local.strategy';
 
 @Module({
-  imports: [UsersService],
-  providers: [AuthService]
+  imports: [UsersService, PassportModule],
+  providers: [AuthService, LocalStrategy]
 })
 export class AuthModule {}
